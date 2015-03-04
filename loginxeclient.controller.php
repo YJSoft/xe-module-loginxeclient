69ea937f5a453d65582bbe8c4af4f610d0585e66
YJSoft <yjsoft@yjsoft.pe.kr>
2015-03-04 16:16:12 +0900

    약관 동의 페이지 추가

--- loginxeclient.controller.php
+++ loginxeclient.controller.php
@@ -16,7 +16,6 @@ class loginxeclientController extends loginxeclient
 
 	function triggerLoginxeclientAddMemberMenu()
 	{
-		debugPrint('ADD MEMBER MENU');
 		$logged_info = Context::get('logged_info');
 		if(!Context::get('is_logged')) return new Object();
 		//$target_srl = Context::get('target_srl');
@@ -31,5 +30,173 @@ class loginxeclientController extends loginxeclient
 		}
 
 		return new Object();
+	}
+
+	function triggerDisablePWChk($args)
+	{
+		$cond = new stdClass();
+		$cond->srl = $args->member_srl;
+		$cond->type = 'naver';
+		$output = executeQuery('loginxeclient.getLoginxeclientMemberbySrl', $cond);
+		if(isset($output->data->enc_id)) $_SESSION['rechecked_password_step'] = 'INPUT_DATA';
+
+		$cond->type = 'github';
+		$output = executeQuery('loginxeclient.getLoginxeclientMemberbySrl', $cond);
+		if(isset($output->data->enc_id)) $_SESSION['rechecked_password_step'] = 'INPUT_DATA';
+		return;
+	}
+
+	function procLoginxeclientOAuthJoin()
+	{
+		$oMemberModel = getModel('member');
+		$config = $oMemberModel->getMemberConfig();
+
+		if($config->agreement && Context::get('accept_loginxe_agreement')!='Y') return $this->stop('msg_accept_agreement');
+
+		if(Context::get('password')!=Context::get('password2')) return $this->stop('loginxecli_incorrectpassword');
+
+		$_SESSION['loginxetemp_joindata']->password = Context::get('password');
+		return $this->_doLoginXEJoin($_SESSION['loginxetemp_joindata']);
+	}
+
+	/*
+	 * $func_arg
+	 * child
+	 *  - email
+	 *  - nick_name
+	 *  - state
+	 *  - enc_id
+	 *  - type
+	 *  - profile
+	 *  - password
+	 */
+	function _doLoginXEJoin($func_arg)
+	{
+		$oMemberModel = getModel('member');
+		$config = $oMemberModel->getMemberConfig();
+		$oMemberController = getController('member');
+		$oLoginXEServerModel = getModel('loginxeclient');
+		$module_config = $oLoginXEServerModel->getConfig();
+
+		// call a trigger (before)
+		$trigger_output = ModuleHandler::triggerCall ('member.procMemberInsert', 'before', $config);
+		if(!$trigger_output->toBool ()) return $trigger_output;
+		// Check if an administrator allows a membership or module config allows external join function
+		if($module_config->loginxe_joinenable=='') $module_config->loginxe_joinenable='true';
+		if($config->enable_join != 'Y' || $module_config->loginxe_joinenable != 'true')
+		{
+			return new Object(-1,'msg_signup_disabled');
+		}
+
+		if($oMemberModel->getMemberInfoByEmailAddress($func_arg->email))
+		{
+			return new Object(-1,'loginxecli_duplicate_email');
+		}
+
+		$args = new stdClass();
+		list($args->email_id, $args->email_host) = explode('@', $func_arg->email);
+		$args->allow_mailing="N";
+		$args->allow_message="Y";
+		$args->email_address=$func_arg->email;
+		$args->find_account_answer=md5($func_arg->state) . '@' . $args->email_host;
+		$args->find_account_question="1";
+		$args->nick_name=$func_arg->nick_name;
+		while($oMemberModel->getMemberSrlByNickName($args->nick_name)){
+			$args->nick_name=$func_arg->nick_name . substr(md5($func_arg->state . rand(0,9999)),0,5);
+		}
+		$args->password=$func_arg->password;
+
+		// check password strength
+		if(!$oMemberModel->checkPasswordStrength($args->password, $config->password_strength))
+		{
+			$message = Context::getLang('about_password_strength');
+			return new Object(-1, $message[$config->password_strength]);
+		}
+
+		$args->user_id=substr($args->email_id,0,20);
+		$useN=FALSE;
+		//if id's first char is number, add n for first char.
+		if(preg_match('/[0-9]/',substr($args->user_id,0,1)))
+		{
+			$useN=TRUE;
+			$args->user_id = 'lxe' . substr($args->email_id,0,17);
+		}
+		while($oMemberModel->getMemberInfoByUserID($args->user_id)){
+			if($useN) $args->user_id = 'lxe' . substr($args->email_id,0,7) . substr(md5($func_arg->state . rand(0,9999)),0,10);
+			else $args->user_id=substr($args->email_id,0,10) . substr(md5($func_arg->state . rand(0,9999)),0,10);
+		}
+		$args->user_name=$func_arg->nick_name;
+
+		// remove whitespace
+		$checkInfos = array('user_id', 'nick_name', 'email_address');
+		$replaceStr = array("\r\n", "\r", "\n", " ", "\t", "\xC2\xAD");
+		foreach($checkInfos as $val)
+		{
+			if(isset($args->{$val}))
+			{
+				$args->{$val} = str_replace($replaceStr, '', $args->{$val});
+			}
+		}
+
+		$output = $oMemberController->insertMember($args);
+		if(!$output->toBool()) return $output;
+
+		$site_module_info = Context::get('site_module_info');
+		if($site_module_info->site_srl > 0)
+		{
+			$columnList = array('site_srl', 'group_srl');
+			$default_group = $oMemberModel->getDefaultGroup($site_module_info->site_srl, $columnList);
+			if($default_group->group_srl)
+			{
+				$oMemberModel->addMemberToGroup($args->member_srl, $default_group->group_srl, $site_module_info->site_srl);
+			}
+
+		}
+
+		$LoginXEMember = new stdClass();
+		$LoginXEMember->srl = $args->member_srl;
+		$LoginXEMember->enc_id = $func_arg->enc_id;
+		$LoginXEMember->type=$func_arg->type;
+
+		$output = executeQuery('loginxeclient.insertLoginxeclientMember', $LoginXEMember);
+		if(!$output->toBool())
+		{
+			return new Object(-1,$output->message);
+		}
+
+		$tmp_file = sprintf('./files/cache/tmp/%d', md5(rand(111111,999999).$args->email_id));
+		if(!is_dir('./files/cache/tmp')) FileHandler::makeDir('./files/cache/tmp');
+
+		$ping_header = array();
+		$ping_header['Pragma'] = 'no-cache';
+		$ping_header['Accept'] = '*/*';
+
+		$request_config = array();
+		$request_config['ssl_verify_peer'] = false;
+
+		FileHandler::getRemoteFile($func_arg->profile, $tmp_file,null, 10, 'GET', null,$ping_header,array(),array(),$request_config);
+
+		if(file_exists($tmp_file))
+		{
+			$oMemberController->insertProfileImage($args->member_srl, $tmp_file);
+		}
+
+		if($config->identifier == 'email_address')
+		{
+			$oMemberController->doLogin($args->email_address);
+		}
+		else
+		{
+			$oMemberController->doLogin($args->user_id);
+		}
+
+		$_SESSION['rechecked_password_step'] = 'INPUT_DATA';
+
+		if($config->redirect_url) $this->redirect_Url = $config->redirect_url;
+		else $this->redirect_Url = getNotEncodedUrl('', 'act', 'dispMemberModifyInfo');
+
+		FileHandler::removeFile($tmp_file);
+
+		return;
 	}
 }
--- loginxeclient.view.php
+++ loginxeclient.view.php
@@ -68,51 +68,37 @@ class loginxeclientView extends loginxeclient
 		Context::set('providers',$logindata);
 	}
 
-	function dispLoginxeclientOAuth()
-	{
-		//oauth display & redirect act
-		//load config here and redirect to service
-		//key check & domain check needed
-		//needed value=service,id,key,state(client-generated),callback-url(urlencoded)
-		$service = Context::get('provider');
-		$oLoginXEServerModel = getModel('loginxeclient');
-		$module_config = $oLoginXEServerModel->getConfig();
-		//설정에서 체크하지 않은 provider일 경우 잘못된 요청입니다 출력
-		if(!in_array($service, $module_config->loginxe_provider))
-		{
-			return new Object(-1,'msg_invalid_request');
-		}
-		//state 생성
-		$_SESSION['loginxecli_state'] = $this->generate_state();
-
-		//서버 주소로 이동
-		Context::set('url',$module_config->loginxe_server . sprintf("/index.php?module=loginxeserver&act=dispLoginxeserverOAuth&provider=%s&id=%s&key=%s&state=%s&callback=%s",$service,$module_config->loginxe_id,$module_config->loginxe_key,$_SESSION['loginxecli_state'],urlencode(getNotEncodedFullUrl('','act','dispLoginxeclientOAuthFinish','provider',$service))));
-	}
-
-	function dispLoginxeclientRevokeProvider()
-	{
-		if(!Context::get('is_logged'))
-		{
-			return new Object(-1,'msg_not_permitted');
-		}
-		$service = Context::get('provider');
-
-		$cond = new stdClass();
-		$cond->srl = Context::get('logged_info')->member_srl;
-		$cond->type = $service;
-		$output = executeQuery('loginxeclient.deleteLoginxeclientProvider', $cond);
-		if(!$output->toBool())
-		{
-			return new Object(-1,$output->message);
-		}
-
-		return new Object(-1,'loginxecli_disconnected');
-
-	}
-
 	function dispLoginxeclientOAuthFinish()
 	{
+		if(Context::get('token')=='') return new Object(-1,'No token given.');
+
 		$oMemberModel = getModel('member');
+		$config = $oMemberModel->getMemberConfig();
+		Context::set('member_config',$config);
+
+		$skin = $oMemberConfig->skin;
+
+		if(!$skin)
+		{
+			$skin = 'default';
+			$template_path = sprintf('./modules/member/skins/%s', $skin);
+		}
+		else
+		{
+			//check theme
+			$config_parse = explode('|@|', $skin);
+			if (count($config_parse) > 1)
+			{
+				$template_path = sprintf('./themes/%s/modules/member/', $config_parse[0]);
+			}
+			else
+			{
+				$template_path = sprintf('./modules/member/skins/%s', $skin);
+			}
+		}
+
+		Context::set('memberskin',$template_path);
+
 		$oMemberController = getController('member');
 		$oLoginXEServerModel = getModel('loginxeclient');
 		$module_config = $oLoginXEServerModel->getConfig();
@@ -216,7 +202,6 @@ class loginxeclientView extends loginxeclient
 				$cond->enc_id=$xmlDoc->data->response->enc_id->body;
 				$cond->type=$service;
 				$output = executeQuery('loginxeclient.getLoginxeclientbyEncID', $cond);
-				$config = $oMemberModel->getMemberConfig();
 
 				//srl이 있다면(로그인 시도)
 				if(isset($output->data->srl))
@@ -258,7 +243,8 @@ class loginxeclientView extends loginxeclient
 					$funcarg->type = $service;
 					$funcarg->profile = $xmlDoc->data->response->profile_image->body;
 
-					$this->_doLoginXEJoin($funcarg);
+					$_SESSION['loginxetemp_joindata'] = $funcarg;
+					return;
 				}
 			}
 		}
@@ -379,7 +365,8 @@ class loginxeclientView extends loginxeclient
 					$funcarg->type = $service;
 					$funcarg->profile = $xmlDoc->avatar_url;
 
-					$this->_doLoginXEJoin($funcarg);
+					$_SESSION['loginxetemp_joindata'] = $funcarg;
+					return;
 				}
 			}
 		}
@@ -395,142 +382,51 @@ class loginxeclientView extends loginxeclient
 		Context::set('url',$this->redirect_Url);
 	}
 
-	function generate_state() {
-		$mt = microtime();
-		$rand = mt_rand();
-		return md5($mt . $rand);
-	}
-
-	/*
-	 * $func_arg
-	 * child
-	 *  - email
-	 *  - nick_name
-	 *  - state
-	 *  - enc_id
-	 *  - type
-	 *  - profile
-	 */
-	function _doLoginXEJoin($func_arg)
+	function dispLoginxeclientOAuth()
 	{
-		$oMemberModel = getModel('member');
-		$config = $oMemberModel->getMemberConfig();
-		$oMemberController = getController('member');
+		//oauth display & redirect act
+		//load config here and redirect to service
+		//key check & domain check needed
+		//needed value=service,id,key,state(client-generated),callback-url(urlencoded)
+		$service = Context::get('provider');
 		$oLoginXEServerModel = getModel('loginxeclient');
 		$module_config = $oLoginXEServerModel->getConfig();
-
-		// call a trigger (before)
-		$trigger_output = ModuleHandler::triggerCall ('member.procMemberInsert', 'before', $config);
-		if(!$trigger_output->toBool ()) return $trigger_output;
-		// Check if an administrator allows a membership or module config allows external join function
-		if($module_config->loginxe_joinenable=='') $module_config->loginxe_joinenable='true';
-		if($config->enable_join != 'Y' || $module_config->loginxe_joinenable != 'true')
+		//설정에서 체크하지 않은 provider일 경우 잘못된 요청입니다 출력
+		if(!in_array($service, $module_config->loginxe_provider))
 		{
-			return new Object(-1,'msg_signup_disabled');
+			return new Object(-1,'msg_invalid_request');
 		}
+		//state 생성
+		$_SESSION['loginxecli_state'] = $this->generate_state();
 
-		if($oMemberModel->getMemberInfoByEmailAddress($func_arg->email))
+		//서버 주소로 이동
+		Context::set('url',$module_config->loginxe_server . sprintf("/index.php?module=loginxeserver&act=dispLoginxeserverOAuth&provider=%s&id=%s&key=%s&state=%s&callback=%s",$service,$module_config->loginxe_id,$module_config->loginxe_key,$_SESSION['loginxecli_state'],urlencode(getNotEncodedFullUrl('','act','dispLoginxeclientOAuthFinish','provider',$service))));
+	}
+
+	function dispLoginxeclientRevokeProvider()
+	{
+		if(!Context::get('is_logged'))
 		{
-			return new Object(-1,'loginxecli_duplicate_email');
+			return new Object(-1,'msg_not_permitted');
 		}
+		$service = Context::get('provider');
 
-		$args = new stdClass();
-		list($args->email_id, $args->email_host) = explode('@', $func_arg->email);
-		$args->allow_mailing="N";
-		$args->allow_message="Y";
-		//$xmlDoc->data->response->email->body;
-		$args->email_address=$func_arg->email;
-		//md5($state) . '@' . $args->email_host;
-		$args->find_account_answer=md5($func_arg->state) . '@' . $args->email_host;
-		$args->find_account_question="1";
-		//$xmlDoc->data->response->nickname->body;
-		$args->nick_name=$func_arg->nick_name;
-		while($oMemberModel->getMemberSrlByNickName($args->nick_name)){
-			$args->nick_name=$func_arg->nick_name . substr(md5($func_arg->state . rand(0,9999)),0,5);
-		}
-		$args->password=md5($func_arg->state) . "a1#";
-		$args->user_id=substr($args->email_id,0,20);
-		$useN=FALSE;
-		//if id's first char is number, add n for first char.
-		if(preg_match('/[0-9]/',substr($args->user_id,0,1)))
-		{
-			$useN=TRUE;
-			$args->user_id = 'lxe' . substr($args->email_id,0,17);
-		}
-		while($oMemberModel->getMemberInfoByUserID($args->user_id)){
-			if($useN) $args->user_id = 'lxe' . substr($args->email_id,0,7) . substr(md5($func_arg->state . rand(0,9999)),0,10);
-			else $args->user_id=substr($args->email_id,0,10) . substr(md5($func_arg->state . rand(0,9999)),0,10);
-		}
-		$args->user_name=$func_arg->nick_name;
-
-		// remove whitespace
-		$checkInfos = array('user_id', 'nick_name', 'email_address');
-		$replaceStr = array("\r\n", "\r", "\n", " ", "\t", "\xC2\xAD");
-		foreach($checkInfos as $val)
-		{
-			if(isset($args->{$val}))
-			{
-				$args->{$val} = str_replace($replaceStr, '', $args->{$val});
-			}
-		}
-
-		$output = $oMemberController->insertMember($args);
-		if(!$output->toBool()) return $output;
-
-		$site_module_info = Context::get('site_module_info');
-		if($site_module_info->site_srl > 0)
-		{
-			$columnList = array('site_srl', 'group_srl');
-			$default_group = $oMemberModel->getDefaultGroup($site_module_info->site_srl, $columnList);
-			if($default_group->group_srl)
-			{
-				$oMemberModel->addMemberToGroup($args->member_srl, $default_group->group_srl, $site_module_info->site_srl);
-			}
-
-		}
-
-		$LoginXEMember = new stdClass();
-		$LoginXEMember->srl = $args->member_srl;
-		$LoginXEMember->enc_id = $func_arg->enc_id;
-		$LoginXEMember->type=$func_arg->type;
-
-		$output = executeQuery('loginxeclient.insertLoginxeclientMember', $LoginXEMember);
+		$cond = new stdClass();
+		$cond->srl = Context::get('logged_info')->member_srl;
+		$cond->type = $service;
+		$output = executeQuery('loginxeclient.deleteLoginxeclientProvider', $cond);
 		if(!$output->toBool())
 		{
 			return new Object(-1,$output->message);
 		}
 
-		$tmp_file = sprintf('./files/cache/tmp/%d', md5(rand(111111,999999).$args->email_id));
-		if(!is_dir('./files/cache/tmp')) FileHandler::makeDir('./files/cache/tmp');
+		return new Object(-1,'loginxecli_disconnected');
 
-		$ping_header = array();
-		$ping_header['Pragma'] = 'no-cache';
-		$ping_header['Accept'] = '*/*';
+	}
 
-		$request_config = array();
-		$request_config['ssl_verify_peer'] = false;
-
-		FileHandler::getRemoteFile($func_arg->profile, $tmp_file,null, 10, 'GET', null,$ping_header,array(),array(),$request_config);
-
-		if(file_exists($tmp_file))
-		{
-			$oMemberController->insertProfileImage($args->member_srl, $tmp_file);
-		}
-
-		if($config->identifier == 'email_address')
-		{
-			$oMemberController->doLogin($args->email_address);
-		}
-		else
-		{
-			$oMemberController->doLogin($args->user_id);
-		}
-
-		$_SESSION['rechecked_password_step'] = 'INPUT_DATA';
-
-		if($config->redirect_url) $this->redirect_Url = $config->redirect_url;
-		else $this->redirect_Url = getNotEncodedUrl('', 'act', 'dispMemberModifyInfo');
-
-		FileHandler::removeFile($tmp_file);
+	function generate_state() {
+		$mt = microtime();
+		$rand = mt_rand();
+		return md5($mt . $rand);
 	}
 }
